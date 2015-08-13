f726eb84298a199837b267f49c2d2cc3f874aa30
Soe Naing Tun(snt) <soenaingtun90@gmail.com>
2015-08-13 15:07:31 +0630

    port test again

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 var express  = require('express');
 var app      = express(); 								// create our app w/ express
 var mongoose = require('mongoose'); 					// mongoose for mongodb
-//var port  	 = process.env.PORT || 8080; 				// set the port
+var port  	 = process.env.PORT || 4000; 				// set the port
 var database = require('./config/database'); 			// load the database config
 var morgan   = require('morgan');
 var bodyParser = require('body-parser');
@@ -23,5 +23,5 @@ app.use(methodOverride('X-HTTP-Method-Override')); // override with the X-HTTP-M
 require('./app/routes.js')(app);
 
 // listen (start app with node server.js) ======================================
-//app.listen(port);
-//console.log("App listening on port " + port);
+app.listen(port);
+console.log("App listening on port " + port);
